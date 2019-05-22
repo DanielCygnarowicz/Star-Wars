43f3707d48b9aca0736d8e5c67dee42269df664d
Daniel Cygnarowicz <daniel.cygnarowicz@onet.eu>
2019-05-22 11:46:51 +0200

    Updates

---
+++ precache-manifest.2fe59d93fdc66cdf797f06647e8d003f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2602bdd76a84e9b96da92cd975fd22",
+    "url": "/Star-Wars/Source/index.html"
+  },
+  {
+    "revision": "f1fbfc5afd8d9292ba0e",
+    "url": "/Star-Wars/Source/static/css/main.2124d961.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Star-Wars/Source/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "f1fbfc5afd8d9292ba0e",
+    "url": "/Star-Wars/Source/static/js/main.6717b933.chunk.js"
+  },
+  {
+    "revision": "a9cbae87c67fe8564f28",
+    "url": "/Star-Wars/Source/static/js/runtime~main.287c63c5.js"
+  },
+  {
+    "revision": "169df909165d7fbe216b5138938ccb44",
+    "url": "/Star-Wars/Source/static/media/blue1024.169df909.png"
+  },
+  {
+    "revision": "16f628b9c3cc6ba081786134a210ca36",
+    "url": "/Star-Wars/Source/static/media/blue480x43.16f628b9.png"
+  },
+  {
+    "revision": "9125e7878894aab8f5bc31744ce3cac2",
+    "url": "/Star-Wars/Source/static/media/blue768.9125e787.png"
+  },
+  {
+    "revision": "6153bd345c330469666c01c10aa8a86b",
+    "url": "/Star-Wars/Source/static/media/red1024x81.6153bd34.png"
+  },
+  {
+    "revision": "f0b9ed49a8bc3feb74327bc2370e78cd",
+    "url": "/Star-Wars/Source/static/media/red768x61.f0b9ed49.png"
+  }
+]);
